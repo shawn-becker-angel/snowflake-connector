2c3e5249ed8bec31892e6b6e56435328d30cfecb
Shawn Becker <shawn.becker@angel.com>
2022-07-08 13:23:15 -0600

    added timeout

--- find_key_tables.py
+++ find_key_tables.py
@@ -1,14 +1,16 @@
 import sys
 # import pandas as pd
 import snowflake.connector as connector
+from snowflake.connector import ProgrammingError
 from constants import *   
 from timefunc import timefunc
 from time import sleep, perf_counter
 from typing import Set, List, Dict
 import datetime
+from functools import cache
 
-timeout = 10
 all_key_columns_set = set(["ANONYMOUS_ID", "USER_ID", "EMAIL"])
+all_key_columns_list = sorted(list(all_key_columns_set))
 
 # Returns True if name contains any filter in filters
 def matches_any(name, filters) -> bool:
@@ -17,24 +19,24 @@ def matches_any(name, filters) -> bool:
             return True
     return False
 
-# Returns a list of all filtered SEGMENT table names that contain all key_columns
+# Returns a list of all filtered SEGMENT table_entries that contain all key_columns
 @timefunc
 def get_SEGMENT_table_entries_with_all_key_columns() -> Set[str]:
-    # set of all filtered table names that contain all key_columns
+    # set of all filtered table_entries that contain all key_columns
     table_entries_with_all_key_columns = set()
 
-    # the number of column names found in a given table
-    table_column_count = {}
+    # the number of column names found in a given table_entry
+    table_entry_column_count = {}
     
-    # the number of times each column name is found among all tables
+    # the number of times each column name is found among all table_entries
     column_name_counts = {}
     
-    # keeps the set of existing key_columns for a given table
-    # { table_name:<table_name>, key_columns_set: (<key_column>...) }
-    table_key_columns_sets = {} 
+    # keeps the set of existing key_columns for a given table_entry
+    # { table_entry:<table_entry>, key_columns_set: (<key_column>...) }
+    table_entry_key_column_sets = {} 
     
     # skip table_names that contain any of the following phrases 
-    table_name_filters = set(["DEV","STAGING"])
+    table_entry_filters = set(["DEV","STAGING"])
     
     batch_size = 10
     try:
@@ -48,11 +50,11 @@ def get_SEGMENT_table_entries_with_all_key_columns() -> Set[str]:
             protocol='https',
             port=PORT)
         
-        print(f"new connection: {WAREHOUSE} {DATABASE} {SCHEMA}")
+        print(f"\nnew connection: {WAREHOUSE} {DATABASE} {SCHEMA}")
 
         cur = conn.cursor()
-        query = "SELECT TABLE_NAME, COLUMN_NAME, IS_NULLABLE, DATA_TYPE FROM LOOKER_SOURCE.INFORMATION_SCHEMA.COLUMNS"
-        cur.execute(query, timeout=timeout)
+        query = "SELECT TABLE_NAME, COLUMN_NAME FROM LOOKER_SOURCE.INFORMATION_SCHEMA.COLUMNS"
+        cur.execute(query)
         
         num_batches = 0
         total_rows = 0
@@ -63,15 +65,25 @@ def get_SEGMENT_table_entries_with_all_key_columns() -> Set[str]:
                 break
             for result_row in result_rows:
                 
-                table_name = result_row[0]
-                table_column_count[table_name] = table_column_count.get(table_name, 0) + 1
+                # table__name: SEGMENT__THE_CHOSEN_APP_WEB_PROD__LIVESTREAM_VIEW_STORE_BTN
+                table__name = result_row[0]
+                
+                # table_entry: SEGMENT.THE_CHOSEN_APP_WEB_PROD.LIVESTREAM_VIEW_STORE_BTN
+                table_entry = table__name.replace("__",".")
+                parts = table_entry.split(".")
+        
+                # set database and schema for table_entry that has only the table part, like 'ANGL_APP_OPN_TO_PIF'
+                if len(parts) == 1:
+                    table_entry = 'LOOKER_SOURCE.PUBLIC.' + table_entry
+                
+                table_entry_column_count[table_entry] = table_entry_column_count.get(table_entry, 0) + 1
                 column_name = result_row[1]
                 column_name_counts[column_name] = column_name_counts.get(column_name, 0) + 1
                 
-                if column_name in all_key_columns_set and not matches_any(table_name, table_name_filters):
-                    key_columns_set = table_key_columns_sets.get(table_name, set())
+                if column_name in all_key_columns_set and not matches_any(table_entry, table_entry_filters):
+                    key_columns_set = table_entry_key_column_sets.get(table_entry, set())
                     key_columns_set.add(column_name)
-                    table_key_columns_sets[table_name] = key_columns_set
+                    table_entry_key_column_sets[table_entry] = key_columns_set
                         
                 print(result_row)
             total_rows += num_result_rows
@@ -80,10 +92,10 @@ def get_SEGMENT_table_entries_with_all_key_columns() -> Set[str]:
         print(f"\nfetched {total_rows} total_rows in {num_batches} batches")
         
         print(f"\nfound {total_rows} columns")
-        print(f"\nfound {len(table_column_count)} tables")
+        print(f"\nfound {len(table_entry_column_count)} tables")
         
-        print("\ntop 10 table column counts")
-        sorted_table_column_counts = sorted(table_column_count.items(), key=lambda e: e[1], reverse=True)
+        print("\ntop 10 table_entry column counts")
+        sorted_table_column_counts = sorted(table_entry_column_count.items(), key=lambda e: e[1], reverse=True)
         for i in range(10):
             print(sorted_table_column_counts[i])
 
@@ -96,17 +108,20 @@ def get_SEGMENT_table_entries_with_all_key_columns() -> Set[str]:
         for key in all_key_columns_set:
             print(f"{key}: {column_name_counts.get(key)}")
         
-        print(f"\n{len(table_key_columns_sets.keys())} tables with any key_columns:", all_key_columns_set)
-        for table_name, table_key_columns_set in table_key_columns_sets.items():
+        print(f"\n{len(table_entry_key_column_sets.keys())} tables with any key_columns:", all_key_columns_set)
+        for table_entry, table_key_columns_set in table_entry_key_column_sets.items():
             if table_key_columns_set == all_key_columns_set:
-                assert table_name not in table_entries_with_all_key_columns, f"ERROR: {table_name} already saved"
-                table_entries_with_all_key_columns.add(table_name)
-                print(table_name)
+                assert table_entry not in table_entries_with_all_key_columns, f"ERROR: {table_entry} already saved"
+                table_entries_with_all_key_columns.add(table_entry)
+                print(table_entry)
         
         return table_entries_with_all_key_columns
         
-    except Exception as err:
-        print(f"Error: {type(err)} {str(err)}")
+    except ProgrammingError as err:
+        if err.errno == 604:
+            print("timeout")
+        else:
+            print(f"Error: {type(err)} {str(err)}")
 
     finally:
         cur.close()
@@ -119,21 +134,21 @@ def parse_key_column_counts(result_row_parts: List[str]) -> Dict[str,str]:
     assert len(key_column_names) == len(key_column_counts), "ERROR: in lengths of keys and values"
     return dict(zip(key_column_names, key_column_counts))
 
-def get_table_entry_query_string(table_entry):
-    key_columns = sorted(list(all_key_columns_set))
-    parts = ["'" + table_entry + "'"]
-    for key in key_columns:
-        parts.append(key)
-    concats = ",'|',".join(parts)
-    select_clause = f"count(distinct(concat({concats})))"
-    table_path = table_entry.replace('__','.')
-    key_columns_where_clause = " and ".join([f"{x} is not NULL" for x in key_columns])
-    query_str = f"SELECT {select_clause} FROM {table_path} WHERE {key_columns_where_clause}"
-    return query_str
+@cache
+def get_table_entry_key_column_counts_query_template():
+    select_clause = ",".join([f"count(distinct({x}))" for x in all_key_columns_list])
+    where_clause = " and ".join([f"{x} is not NULL" for x in all_key_columns_list])
+    query_template = f"SELECT {select_clause} FROM <<table_entry>> WHERE {where_clause}"
+    return query_template
 
+def get_table_entry_key_column_counts_query(table_entry):
+    template = get_table_entry_key_column_counts_query_template()
+    return template.replace("<<table_entry>>",table_entry)
 
-# Return list of utc timestamped key_column_counts for all SEGMENT_table_entries. for example:
-#
+# Return list of utc timestamped key_column_counts for all SEGMENT_table_entries.
+# Run and log the results of this task periodically to compare growth rates of key_column counts in tables over time.
+# 
+# Structure:
 # table_entries_key_column_counts = [ 
 #  { 
 #    datetime: 2022-07-07T21:43:16.011804, 
@@ -150,23 +165,10 @@ def get_table_entry_query_string(table_entry):
 def get_SEGMENT_table_entries_key_column_counts(table_entries_with_all_key_columns: Set[str]) -> List[Dict]:
     table_entries_key_column_counts = []
     
-    job_datetime_str = datetime.datetime.utcnow().isoformat();
     sorted_table_entries = sorted(list(table_entries_with_all_key_columns))
     uncounted_table_entries = set(sorted_table_entries)
     counted_table_entries = set()
-    
-    parts = []
-    for key in list(all_key_columns_set):
-        parts.append(f"COUNT(DISTINCT({key}))")
-    key_columns_select_clause = ", ".join(parts)
-    
-    parts = []
-    for key in list(all_key_columns_set):
-        parts.append( f"{key} is not NULL" )
-    key_columns_where_clause = " and ".join(parts)
-    
-    batch_size = 10
-    
+        
     conn = connector.connect(
         user=USER_NAME,
         password=USER_PSWD,
@@ -176,21 +178,16 @@ def get_SEGMENT_table_entries_key_column_counts(table_entries_with_all_key_colum
         schema=SCHEMA,
         protocol='https',
         port=PORT)
-    cur = None
-    
-    for table_entry in sorted_table_entries:
+    print(f"\nnew connection: {WAREHOUSE} {DATABASE} {SCHEMA}")
         
-        table_entry = table_entry.replace("__",".")
-        parts = table_entry.split(".")
-        
-        # set database and schema for entries that have only table, like 'ANGL_APP_OPN_TO_PIF'
-        if len(parts) == 1:
-            table_entry = 'LOOKER_SOURCE.PUBLIC.' + table_entry
-            
+    for table_entry in sorted_table_entries:  
+        query = get_table_entry_key_column_counts_query(table_entry) 
         try:
-            query = f"SELECT {key_columns_select_clause} FROM {table_entry} WHERE {key_columns_where_clause}"
+            batch_size = 10
+            timeout_seconds = 10
+            
             cur = conn.cursor()
-            cur.execute(query, timeout=timeout)
+            cur.execute(query, timeout=timeout_seconds)
 
             num_batches = 0
             total_rows = 0
@@ -203,7 +200,7 @@ def get_SEGMENT_table_entries_key_column_counts(table_entries_with_all_key_colum
                 for result_row_parts in result_rows:
                     key_column_counts =  parse_key_column_counts(result_row_parts)
                     table_entry_key_column_counts = {
-                        "datetime": job_datetime_str,
+                        "datetime": datetime.datetime.utcnow().isoformat(),
                         "table_entry": table_entry,
                         "key_column_counts": key_column_counts
                     }
@@ -217,16 +214,19 @@ def get_SEGMENT_table_entries_key_column_counts(table_entries_with_all_key_colum
             counted_table_entries.add(table_entry)
             uncounted_table_entries.remove(table_entry)
             
-        except Exception as err:
-            print(f"Error: {type(err)} {str(err)}")
+        except ProgrammingError as err:
+            if err.errno == 604:
+                print(timeout_seconds, "second timeout for query:\n", query)
+            else:
+                print(f"Error: {type(err)} {str(err)}")
         finally:
             cur.close()
     # end for table_entry
     
-    # display query strings for all skipped table entries
-    print("\nquery strings for", len(uncounted_table_entries), "skipped tables out of", len(sorted_table_entries) )
+    # display query strings for all skipped table_entries
+    print("\nquery strings for", len(uncounted_table_entries), "skipped table_entries out of", len(sorted_table_entries) )
     for table_entry in sorted(uncounted_table_entries):
-        query_string = get_table_entry_query_string(table_entry)
+        query_string = get_table_entry_key_column_counts_query(table_entry)
         print(query_string + ';')
 
     print("\npassed", len(counted_table_entries), "tables out of", len(sorted_table_entries) )
@@ -251,27 +251,30 @@ def get_SEGMENT_table_entries_key_column_counts(table_entries_with_all_key_colum
 #   },
 #   ...
 # ]
+@timefunc
 def get_SEGMENT_key_column_values(table_entries_key_column_counts):
     key_column_values = []
     return key_column_values
 
-def test_get_table_entry_query_string():
-    table_entry = 'ANGL_APP_OPN_TO_PIF_GNRL'
-    expected = "SELECT distinct(concat('ANGL_APP_OPN_TO_PIF_GNRL','|','ANONYMOUS_ID','|','EMAIL','|','USER_ID')) FROM ANGL_APP_OPN_TO_PIF_GNRL WHERE ANONYMOUS_ID is not NULL and EMAIL is not NULL and USER_ID is not NULL"
-    result = get_table_entry_query_string(table_entry)
+@timefunc
+def test_get_table_entry_key_column_counts_query() -> bool:
+    table_entry = 'LOOKER_SOURCE.PUBLIC.ANGL_APP_OPN_TO_PIF_GNRL'
+    expected = "SELECT count(distinct(ANONYMOUS_ID)),count(distinct(EMAIL)),count(distinct(USER_ID)) FROM LOOKER_SOURCE.PUBLIC.ANGL_APP_OPN_TO_PIF_GNRL WHERE ANONYMOUS_ID is not NULL and EMAIL is not NULL and USER_ID is not NULL"
+    result = get_table_entry_key_column_counts_query(table_entry)
     assert result == expected, f"ERROR: expected:\n{expected}\nnot result:\n{result}"
     
-    table_entry = 'SEGMENT__ANGEL_MOBILE_ANDROID_PROD__USER_SIGN_IN_STARTED'
-    expected = "SELECT distinct(concat('SEGMENT__ANGEL_MOBILE_ANDROID_PROD__USER_SIGN_IN_STARTED','|','ANONYMOUS_ID','|','EMAIL','|','USER_ID')) FROM SEGMENT.ANGEL_MOBILE_ANDROID_PROD.USER_SIGN_IN_STARTED WHERE ANONYMOUS_ID is not NULL and EMAIL is not NULL and USER_ID is not NULL"
-    result = get_table_entry_query_string(table_entry)
+    table_entry = 'SEGMENT.ANGEL_MOBILE_ANDROID_PROD.USER_SIGN_IN_STARTED'
+    expected = "SELECT count(distinct(ANONYMOUS_ID)),count(distinct(EMAIL)),count(distinct(USER_ID)) FROM SEGMENT.ANGEL_MOBILE_ANDROID_PROD.USER_SIGN_IN_STARTED WHERE ANONYMOUS_ID is not NULL and EMAIL is not NULL and USER_ID is not NULL"
+    result = get_table_entry_key_column_counts_query(table_entry)
     assert result == expected, f"ERROR: expected:\n{expected} not result:\n{result}"
 
-    print("passed")
+    print("all tests passed")
+    return True
 
-    
 
+@timefunc
 def main():
-    # test_get_table_entry_query_string()
+    assert test_get_table_entry_key_column_counts_query() == True, "ERROR: all tests did not pass"
     
     table_entries_with_all_key_columns = get_SEGMENT_table_entries_with_all_key_columns()
     print("\nnum SEGMENT table_entries_with_all_key_columns:", len(table_entries_with_all_key_columns))
